335318391a981694863a9dae41d9c0d33ddc1681
sourav jain <95373772+souravgreatest@users.noreply.github.com>
2025-03-17 17:56:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
 //your JS code here. If required.
+document.getElementById('userform').addEventListener('submit',(e)=>{
+	e.preventDefault();
+	const age=document.getElementById('age').value
+	const name=document.getElementById('name').value
+	if(!name || !age)
+	{
+		alert('Please enter valid details.')
+		return
+	}
+	function checkAge() {
+        return new Promise((resolve, reject) => {
+          setTimeout(() => {
+            if (age > 18) {
+              resolve(`Welcome, ${name}. You can vote.`);
+            } else {
+              reject(`Oh sorry, ${name}. You aren't old enough.`);
+            }
+          }, 4000);
+        });
+      }
+	checkAge()
+		.then((message)=>alert(message))
+		.catch((error)=>alert(error))
+})
+
+
+
